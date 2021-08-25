8beb333020d9ce98fc0368150ba7586fb6f4e327
Kevin Do <kevind0501@gmail.com>
2021-08-25 11:24:08 -0700

    styles

---
+++ app.js
@@ -0,0 +1,20 @@
+// get the element that holds the nametag name
+const nameElement = document.getElementById('name');
+
+// get the button
+const updateButton = document.getElementById('update-button');
+// get the input
+const nameInput = document.getElementById('name-input')
+console.log(updateButton);
+
+// user clicks on the button
+updateButton.addEventListener('click', () => {
+
+    // get the text in the input field
+    const name = nameInput.value;
+
+    // update the name element with the text
+    nameElement.textContent = name;
+});
+
+    
