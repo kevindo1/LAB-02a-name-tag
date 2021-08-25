ceffc8db224b3182e7796bd62d30c35040424f09
Kevin Do <kevind0501@gmail.com>
2021-08-25 12:24:42 -0700

    Added color background buttons

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const nameElement = document.getElementById('name');
 
 // get the button
 const updateButton = document.getElementById('update-button');
+
 // get the input
 const nameInput = document.getElementById('name-input');
-console.log(updateButton);
 
 // user clicks on the button
 updateButton.addEventListener('click', () => {
@@ -17,4 +17,42 @@ updateButton.addEventListener('click', () => {
     nameElement.textContent = name;
 });
 
+// get the element that holds the color value
+const colorElement = document.getElementById('middle');
+
+// get the three buttons
+const pinkButton = document.getElementById('pink');
+const greenButton = document.getElementById('lightgreen');
+const blueButton = document.getElementById('lightblue');
+const reset = document.getElementById('reset');
+
+// user clicks on the Pink button
+pinkButton.addEventListener('click', () => {
     
+    // update background with new color
+    document.getElementById('background').style.backgroundColor = 'pink';
+    
+});
+
+// user clicks on the Light Green button
+greenButton.addEventListener('click', () => {
+    
+    // update background with new color
+    document.getElementById('background').style.backgroundColor = 'lightgreen';
+    
+});
+
+// user clicks on the Light Blue button
+blueButton.addEventListener('click', () => {
+    
+    // update background with new color
+    document.getElementById('background').style.backgroundColor = 'aqua';
+    
+});
+
+//user clicks on the Reset button
+reset.addEventListener('click', () => {
+
+    // reset background color to white
+    document.getElementById('background').style.background = 'white';
+});
